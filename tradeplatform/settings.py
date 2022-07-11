f3b7af00e2a67f30404700624bfc05bb26e4a239
seanwambua - NTC <nairobi.techcreative@gmail.com>
2022-07-11 05:00:42 +0300

    Fixed Static Files Issue and added site updates

--- tradeplatform/settings.py
+++ tradeplatform/settings.py
@@ -129,8 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+STATICFILES_DIRS = ((os.path.join(BASE_DIR, 'static')), )
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
