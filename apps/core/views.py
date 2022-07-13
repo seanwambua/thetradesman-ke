ee4fccd763827e7d7cad4afcf972a55b9d724906
seanwambua - NTC <nairobi.techcreative@gmail.com>
2022-07-13 15:15:21 +0300

    UI Update:     Homepage - List latest items per category

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    newest_products = Products.objects.all()
-    return render(request, 'core/home.html', {'newest_products': newest_products})
+    latest_services = Products.objects.filter(category=1)[0:6]
+    newest_products = Products.objects.filter(category=2)[0:6]
+    latest_ventures = Products.objects.filter(category=3)[0:6]
+    return render(request, 'core/home.html', {'newest_products': newest_products, 'latest_services': latest_services, 'latest_ventures': latest_ventures})
 
 
 def contact(request):
